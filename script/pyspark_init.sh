943a2ab79af57673fe8f1ab1549630364e2d9477
yann223 <55059230+yann223@users.noreply.github.com>
2024-05-07 19:06:31 +0200

    Update pyspark_init.sh

--- script/pyspark_init.sh
+++ script/pyspark_init.sh
@@ -1,6 +1,7 @@
 #!bin/bash
+ln -fs /usr/share/zoneinfo/Europe/Paris /etc/localtime
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get install -y pandoc
-export DEBIAN_FRONTEND=noninteractive
 sleep 10
 sudo apt-get install -y texlive-xetex texlive-fonts-recommended texlive-plain-generic
