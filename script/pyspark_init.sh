a7a57124b783fb55b76d70a83231f77eab4131e9
yann223 <55059230+yann223@users.noreply.github.com>
2024-05-07 18:27:26 +0200

    Update pyspark_init.sh

--- script/pyspark_init.sh
+++ script/pyspark_init.sh
@@ -1,7 +1,10 @@
 #!bin/bash
 sudo apt-get update
 sudo apt-get install -y pandoc
-sleep 60
+sleep 10
 sudo apt-get install -y texlive-xetex texlive-fonts-recommended texlive-plain-generic
-sudo apt-get install -y texlive-fonts-recommended
-sudo apt-get install -y texlive-plain-generic
+sleep 100
+echo 8
+sleep 3
+echo 37
+sleep 60
